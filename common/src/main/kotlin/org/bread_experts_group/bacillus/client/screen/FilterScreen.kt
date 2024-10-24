4b2a429be68c62e3f8f94426dcb24bd4105c4259
ttttdoy <logan3mclean@gmail.com>
2024-10-23 21:58:05 -0400

    finish merge

--- common/src/main/kotlin/org/bread_experts_group/bacillus/client/screen/FilterScreen.kt
+++ common/src/main/kotlin/org/bread_experts_group/bacillus/client/screen/FilterScreen.kt
@@ -1,9 +1,8 @@
 package org.bread_experts_group.bacillus.client.screen
 
-import com.mojang.blaze3d.systems.RenderSystem
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
-import net.minecraft.client.renderer.GameRenderer
+import net.minecraft.client.renderer.RenderType
 import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.player.Inventory
 import org.bread_experts_group.bacillus.Bacillus.modLocation
@@ -17,10 +16,6 @@ class FilterScreen(
     val texture = modLocation("gui", "container", "filter.png")
 
     override fun renderBg(guiGraphics: GuiGraphics, partialTick: Float, mouseX: Int, mouseY: Int) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader)
-        RenderSystem.setShaderColor(1f, 1f, 1f, 1f)
-        RenderSystem.setShaderTexture(0, texture)
-
-        guiGraphics.blit(texture, leftPos, topPos, 0, 0, imageWidth, imageHeight)
+        guiGraphics.blit(RenderType::guiTextured, texture, leftPos, topPos, 0f, 0f, imageWidth, imageHeight, 256, 256)
     }
 }
